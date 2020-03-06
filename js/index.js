1520b2b32bb4f9e6a8bf8b4facb2a6ec3947688a
Rocco <graphic@IMM-Instructor.local>
2020-03-06 15:40:43 -0500

    Some practice stuff.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
+let $bar = document.querySelector('.progress')
 
+
+window.addEventListener('scroll', event => {
+  // Window height
+  let windowH = window.innerHeight
+  // Document height
+  let documentH = document.documentElement.scrollHeight
+  // How much has been scrolled
+  let amtScrolled = window.scrollY
+  // How much CAN be scrolled
+  let ttlAvailable = documentH - windowH 
+  // What percentage of the scrollable is scrolled
+  let percent = amtScrolled / ttlAvailable  // 0.5
+
+  // Take the decimal percentage and convert to /100%
+  $bar.style.width = `${percent * 100}%`  // 50%
+  $bar.textContent = `${Math.round(percent * 100)}%`
+})
+
+
+window.addEventListener('mousemove', event=> {
+
+  document.body.innerHTML += `<div class="dot" style="top:${event.clientY}px; left:${event.clientX}px;"></div>`
+
+})
